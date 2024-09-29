48c09cf0d0bb652bec8783e70098d1dcce500054
Ian Setia <144347412+Ian2327@users.noreply.github.com>
2024-09-29 17:17:26 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const menuButton = document.getElementsByClassName('menu-button')[0]
-const navLinks = document.getElementsByClassName('nav-links')[0]
-
-menuButton.addEventListener('click', () => {
-    navLinks.classList.toggle('active')
-    menuButton.classList.toggle('close')
-})
-
